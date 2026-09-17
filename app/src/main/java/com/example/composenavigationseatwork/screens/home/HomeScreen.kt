15ee2d6321483311891c940817a5e3b4a773d91b
Kurt Sarmiento <sarmijustine@gmail.com>
2026-09-17 10:38:57 +0800

    act 2: done with parts 1, 2, and 3

--- app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
@@ -3,12 +3,15 @@ package com.example.composenavigationseatwork.screens.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -19,25 +22,53 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun HomeScreen(onNavigate: () -> Unit) {
     var loginAttempts by remember {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
+    }
+    var studentId by remember{
+        mutableStateOf("1001")
+    }
+    var name by remember{
+        mutableStateOf("Juan Dela Cruz")
+    }
+    var course by remember{
+        mutableStateOf("BS Computer Engineering")
+    }
+    var year by remember{
+        mutableStateOf("4")
     }
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
     ) {
+
         Text(
             text = "Student Information",
             style = MaterialTheme.typography.headlineLarge
         )
-        Text(
-            text = "Name: Juan Dela Cruz"
+        OutlinedTextField(
+            value = studentId,
+            onValueChange = {studentId = it},
+            label = {Text("Student ID:")},
+            modifier = Modifier.fillMaxWidth()
         )
-        Text(
-            text = "Course: BS Computer Engineering"
+        OutlinedTextField(
+            value = name,
+            onValueChange = {name = it},
+            label = {Text("Name:")},
+            modifier = Modifier.fillMaxWidth()
         )
-        Text(
-            text = "Year: 4"
+        OutlinedTextField(
+            value = course,
+            onValueChange = {course = it},
+            label = {Text("Course:")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = year,
+            onValueChange = {year = it},
+            label = {Text("Year Level:")},
+            modifier = Modifier.fillMaxWidth()
         )
         Text(
             text = "Login Attempts: $loginAttempts"
