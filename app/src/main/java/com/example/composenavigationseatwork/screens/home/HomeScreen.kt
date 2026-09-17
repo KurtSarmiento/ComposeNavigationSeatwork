c86f17b2e0ca535952c13461ed7503e809364718
Kurt Sarmiento <sarmijustine@gmail.com>
2026-09-17 11:14:18 +0800

    act 2: done with part 6

--- app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composenavigationseatwork/screens/home/HomeScreen.kt
@@ -2,13 +2,17 @@ package com.example.composenavigationseatwork.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -17,35 +21,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 @Composable
 fun HomeScreen(onNavigate: (studentId : String, name : String, course: String, year : String) -> Unit) {
-    var loginAttempts by remember {
-        mutableIntStateOf(0)
-    }
-    var studentId by remember {
-        mutableStateOf("")
-    }
-    var name by remember {
-        mutableStateOf("")
-    }
-    var course by remember {
-        mutableStateOf("")
-    }
-    var year by remember {
-        mutableStateOf("")
-    }
-    var errorMessage by remember {
-        mutableStateOf("")
-    }
+    var loginAttempts by remember { mutableIntStateOf(0) }
+    var studentId by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+    var course by remember { mutableStateOf("") }
+    var year by remember { mutableStateOf("") }
+    var errorMessage by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
     ) {
-
         Text(
             text = "Student Information",
             style = MaterialTheme.typography.headlineLarge
@@ -103,11 +96,47 @@ fun HomeScreen(onNavigate: (studentId : String, name : String, course: String, y
                     errorMessage = "Year Level is required"
                 } else {
                     errorMessage = ""
-                    onNavigate(studentId, name, course, year)
+                    showDialog = true
                 }
             }
         ) {
             Text("View Details")
+        }
+        if(showDialog){
+            AlertDialog(
+                onDismissRequest = {
+                    showDialog = false
+                },
+                title = {
+                    Text("Confirm Student Information")
+                },
+                text = {
+                    Column() {
+                        Text("View Details for:")
+                        Spacer(Modifier.height(4.dp))
+                        Text("$name?")
+                    }
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            showDialog = false
+                            onNavigate(studentId, name, course, year)
+                        }
+                    ){
+                        Text("Continue")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                        }
+                    ) {
+                        Text("Cancel")
+                    }
+                }
+            )
         }
     }
 }
